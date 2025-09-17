cf4a8f204a355066efb752318f13cd417dd6e543
Valky <acarrillo.vlk@gmail.com>
2025-09-16 19:12:02 -0600

    feat: inicializar array amigos para almacenar nombres (#1)

--- amigo-secreto_esp/app.js
+++ amigo-secreto_esp/app.js
@@ -1,1 +1,3 @@
+// Array para almacenar los nombres de los amigos
+let amigos = [];
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
